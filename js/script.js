5b9c3c9195112f0145103efe9453f7b2c0a5c6b5
Sergio Gabriel <sgabriel.cafe@gmail.com>
2022-07-22 14:35:40 -0300

    update1.2

--- js/script.js
+++ js/script.js
@@ -1,6 +1,10 @@
 function scrollHeader() {
   const nav = document.getElementById("header");
 
+  //Se o scrollY for > que 50, adiciona a classe de rolagem, caso não seja, remove
   if (this.scrollY >= 50) nav.classList.add("active-header");
   else nav.classList.remove("active-header");
 }
+
+//o scroll é o evento e o scrollHeader é a função do evento
+window.addEventListener("scroll", scrollHeader);
